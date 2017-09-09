ef93457ab31fd8bb80efefa62bb50bf963d43361
mackswane <31156711+mackswane@users.noreply.github.com>
2017-09-09 18:28:51 +0530

    Create lab4 q1.cpp

---
+++ lab4 q1.cpp
@@ -0,0 +1,20 @@
+# lab4
+//ques 1
+#include <iostream>
+#include<cmath>
+
+using namespace std;
+
+void cube()
+     {
+     float n,c;
+     cout<<"\nenter any number";
+     cin>>n;
+     c=pow(n,3);
+     cout<<"\ncube="<<c;
+     }
+int main()
+{
+    cube();
+    return 0;
+}
