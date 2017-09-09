66134cd0143151a1759a8a76557c0a91ba8eae08
mackswane <31156711+mackswane@users.noreply.github.com>
2017-09-09 18:34:13 +0530

    Create lab4 q2.cpp

---
+++ lab4 q2.cpp
@@ -0,0 +1,24 @@
+# lab4
+//ques 2
+#include <iostream>
+#include<cmath>
+
+using namespace std;
+
+void circle()
+     {
+     float r,d,a,c;
+     cout<<"\nenter radius of circle";
+     cin>>r;
+     d=2*r;
+     c=2*3.14*r;
+     a=3.14*r*r;
+     cout<<"\ndiameter="<<d;
+     cout<<"\ncircumference="<<c;
+     cout<<"\narea="<<a;
+     }
+int main()
+{
+    circle();
+    return 0;
+}
